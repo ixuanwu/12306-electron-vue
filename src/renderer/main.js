786a72437785b2e4b0c12cf52f39b1d9a6e0dde0
jianghechao <jianghechao@hotmail.com>
2017-12-03 21:54:11 +0800

    init

---
+++ src/renderer/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  ...App,
+  router,
+  store,
+  el:'#app'
+})
